091a6aace30fbdde314d15165e4c9f6cd44b4a6b
sheboyganmosque <sheboyganmosque@gmail.com>
2024-07-26 16:26:34 -0500

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm('service_ubvg252', 'template_8t8tr7z', this)
+        .then(function() {
+            alert('Message sent!');
+        }, function(error) {
+            alert('Failed to send message. Please try again later.');
+        });
+});
